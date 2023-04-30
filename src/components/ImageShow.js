bf010546afee8ad4ccd7d438d010f49df87de222
rabinkumarbaniya <rabinkrbaniya09@gmail.com>
2023-04-30 20:03:50 +0545

    displayed images

--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const ImageShow = ({ image }) => {
     return (
-        <div>{image.id}</div>
+        <div>
+            <img src={image.urls.small} alt={image.alt_description} />
+        </div>
     )
 }
 
-export default ImageShow;
+export default ImageShow; 
